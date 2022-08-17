25f45d0fc5b82af2a00caa28c75b3e9a08c3a761
Milan Lesichkov <lesichkovm@gmail.com>
2022-08-17 07:02:05 +0100

    Update Icon_test.go

--- Icon_test.go
+++ Icon_test.go
@@ -1,1 +1,13 @@
 package icons
+
+package utils
+
+import "testing"
+
+func TestIcon(t *testing.T) {
+  iconHtml := Icon("bi-globe", 120, 120, "white").ToHtml()
+
+  if strings.Contain(iconHtml, "test") {
+		t.Errorf("Icon does not return expected result '01', but '%s'", iconHtml)
+	}
+}
